daa0ff32d60439a681d100a4e20711ca8b61403d
Giuseppe Calabrò <calabro.giuseppe98@gmail.com>
2023-06-02 14:09:16 +0200

    modified name of env variables

--- src/API/API.ts
+++ src/API/API.ts
@@ -1,7 +1,7 @@
 // API COMUNI E PAESI ITALIANI
 
-export const API_COMUNI:string = `http://api.geonames.org/searchJSON?country=it&lang=it&maxRows=1000&username=${process.env.keyComuni}`
+export const API_COMUNI:string = `http://api.geonames.org/searchJSON?country=it&lang=it&maxRows=1000&username=${process.env.KEY_COMUNI}`
 
 // API METEO
 
-export const API_METEO:string = `http://api.openweathermap.org/data/2.5/weather?APPID=${process.env.keyMeteo}&q=`
+export const API_METEO:string = `http://api.openweathermap.org/data/2.5/weather?APPID=${process.env.KEY_METEO}&q=`
