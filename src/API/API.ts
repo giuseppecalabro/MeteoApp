b65a62dfd38ccb2244080b2a2c1cdd851d171fc2
Giuseppe Calabrò <calabro.giuseppe98@gmail.com>
2023-06-03 11:56:49 +0200

    removed .env test

--- src/API/API.ts
+++ src/API/API.ts
@@ -1,7 +1,7 @@
 // API COMUNI E PAESI ITALIANI
-const keyComuni:string= process.env.KEY_COMUNI!
+const keyComuni:string= process.env.REACT_APP_KEY_COMUNI!
 export const API_COMUNI:string = `http://api.geonames.org/searchJSON?country=it&lang=it&maxRows=1000&username=${keyComuni}`
 
 // API METEO
-const keyMeteo:string = process.env.KEY_COMUNI!
+const keyMeteo:string = process.env.REACT_APP_KEY_METEO!
 export const API_METEO:string = `http://api.openweathermap.org/data/2.5/weather?APPID=${keyMeteo}&q=`
