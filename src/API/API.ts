4aa4cbc9d9800a4f0142cb93cc410ec375efd697
giuseppecalabro <123665397+giuseppecalabro@users.noreply.github.com>
2023-06-04 12:41:57 +0200

    Update API.ts

--- src/API/API.ts
+++ src/API/API.ts
@@ -1,7 +1,7 @@
 // API COMUNI E PAESI ITALIANI
 const keyComuni:string= process.env.REACT_APP_KEY_COMUNI!
-export const API_COMUNI:string = `http://api.geonames.org/searchJSON?country=it&lang=it&maxRows=1000&username=${keyComuni}`
+export const API_COMUNI:string = `https://api.geonames.org/searchJSON?country=it&lang=it&maxRows=1000&username=${keyComuni}`
 
 // API METEO
 const keyMeteo:string = process.env.REACT_APP_KEY_METEO!
-export const API_METEO:string = `http://api.openweathermap.org/data/2.5/weather?APPID=${keyMeteo}&q=`
+export const API_METEO:string = `https://api.openweathermap.org/data/2.5/weather?APPID=${keyMeteo}&q=`
